cc3f794bc86cc48d420465b96125db7d5f943ab9
yuvaraj25082004 <122504399+yuvaraj25082004@users.noreply.github.com>
2024-02-27 09:00:29 +0530

    Add files via upload

---
+++ EXPERIMENT-3.R
@@ -0,0 +1,8 @@
+#Write a R program to create a sequence of numbers from 20 to 50 and find the mean of numbers 
+#from 20 to 60 and sum of numbers from 51 to 91.
+print("sequence of number from 20 to 50")
+print(seq(20,50))
+print("mean of number from 30 to 60")
+print(seq(20,60))
+print("sum of number from 51 to 91")
+print(seq(51,91))
